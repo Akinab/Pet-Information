154023e3691bf524d80be1bf05ec0257a6b62478
Vanica Lobusta <132590418+Akinab@users.noreply.github.com>
2023-07-06 17:40:42 +0800

    initializing the information needeed to fil out by users

--- pet.py
+++ pet.py
@@ -6,3 +6,7 @@ def details ():
 details ()
 
 class Pet:
+    def __init__(self, name, animal_type, age):
+        self.__name = name
+        self.__animal_type = animal_type
+        self.__age = age
